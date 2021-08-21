ad6d3483d82be6b60f8b4bcb808e2a84dda61fe3
vvikulin <1234qwer>
2021-08-21 16:07:05 +0300

    first init

---
+++ src/main/java/ru/insoft/rgali/RgaliApplication.java
@@ -0,0 +1,13 @@
+package ru.insoft.rgali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RgaliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RgaliApplication.class, args);
+	}
+
+}
